13910328b21fac9a42140c89bb92c8567bdd24a7
UtkuEfeAyan <76896911+UtkuEfeAyan@users.noreply.github.com>
2026-07-11 20:03:01 -0700

    ClickerScript

--- Assets/Scripts/Wrath (Red)/ClickTarget.cs
+++ Assets/Scripts/Wrath (Red)/ClickTarget.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ClickTarget : MonoBehaviour
+
+public class ClickTarget : MonoBehaviour, IPointerDownHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    WrathGameManager gameManager;
+    RectTransform rt;
+    Image image;
+    
+    void Awake()
     {
-        
+        rt = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init(WrathGameManager manager, Vector2 anchoredPos)
     {
+        gameManager = manager;
+        rt.anchoredPosition = anchoredPos;
         
+        // Debug.Log("Target spawned at: " + anchoredPos);
+    }
+      
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // Debug.Log("Target clicked!");
+        gameManager.RegisterClick();
+
+        // visual feedback
+        StopAllCoroutines();
+        StartCoroutine(PunchScale());
+    }
+
+    System.Collections.IEnumerator PunchScale()
+    {
+        rt.localScale = Vector3.one * 0.9f;
+        yield return new WaitForSeconds(0.05f);
+        rt.localScale = Vector3.one;
     }
 }
