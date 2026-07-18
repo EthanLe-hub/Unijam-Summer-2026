d953b3faa79921806f20add364f435c0d9a4b2e4
Ethan Le <enathanle@gmail.com>
2026-07-18 01:34:59 -0700

    Built the level design for Lucifer. Next to-do: add a skip button for the dialogue.

--- Assets/Scripts/Pride (Black_White)/LavaBubbleSpawn.cs
+++ Assets/Scripts/Pride (Black_White)/LavaBubbleSpawn.cs
@@ -14,16 +14,21 @@ public class LavaBubbleSpawn : MonoBehaviour
     [SerializeField] Lava lavaScript; // Assign in Unity so it can be passed into every spawned LavaBubble.cs instance. 
 
     float secondsPassed = 0f; // Counts how many seconds of the game have passed. 
-    float scalar = 0.05f; // Multiplier to adjust secondsPassed in relation to graviational multiplier. 
+    float scalar = 0.03f; // Multiplier to adjust secondsPassed in relation to graviational multiplier. 
     float gravityMultiplier; 
 
     [SerializeField] float startingSpawnTime = 4f; // Original spawn time. 
     float spawnTimer; // Time to spawn the lava bubbles (gets shorter as the game does on). 
     float calculatedDelay; // Rate that a lava bubble spawns.
 
+    float maxSecondsPassed = 65f; 
+    float maxDelay = 2.0f; 
+    float maxGravityMult = 0.4f;
+
     void Start()
     {
         spawnTimer = startingSpawnTime; 
+        gravityMultiplier = maxGravityMult; 
     }
 
     void Update()
@@ -35,24 +40,31 @@ public class LavaBubbleSpawn : MonoBehaviour
 
         if (timerRecord.recordTime > 0f)
         {
-            secondsPassed = timerRecord.recordTime; // Get the number of seconds that have passed.  
+            secondsPassed = Mathf.Min(timerRecord.recordTime, maxSecondsPassed); // Get the number of seconds that have passed.  
 
-            calculatedDelay = (60f - timerRecord.recordTime) * scalar; // Rate that a lava bubble spawns. 
+            calculatedDelay = secondsPassed * scalar; // Rate that a lava bubble spawns. 
 
-            if (calculatedDelay < 0.75f) // Cap is 0.75 seconds per coin spawn. 
+            if (calculatedDelay >= maxDelay) // Cap is 2-second reduction per lava bubble spawn. 
             {
-                calculatedDelay = 0.75f; 
+                calculatedDelay = maxDelay; 
             }
 
-            spawnTimer -= Time.deltaTime; // Count down the spawn timer (until it hits 0, which is when we spawn another coin). 
-
-            gravityMultiplier = secondsPassed * scalar; // Increase the gravity multiplier based on time left (cap at 0.75 seconds). 
-
-            if (spawnTimer <= 0f) // Once the spawn timer hits 0 seconds, it is time to spawn another coin. 
+            // Gravity needs to be at least 1 in order for the lava bubble to drop:
+            //gravityMultiplier = 1f + (secondsPassed * scalar); // Increase the gravity multiplier based on time left (cap after 2 seconds).
+/*
+            if (gravityMultiplier >= maxGravityMult)
+            {
+                gravityMultiplier = maxGravityMult; 
+            }
+*/
+            spawnTimer -= Time.deltaTime; // Count down the spawn timer (until it hits 0, which is when we spawn another lava bubble).  
+            
+            if (spawnTimer <= 0f) // Once the spawn timer hits 0 seconds, it is time to spawn another lava bubble. 
             {
                 SpawnLavaBubble();
 
-                spawnTimer = calculatedDelay; // Restart the cycle of the delay until the next coin is supposed to spawn. 
+                // New spawn time is reduced from the initial length: 
+                spawnTimer = startingSpawnTime - calculatedDelay; // Restart the cycle of the delay until the next lava bubble is supposed to spawn. 
             }
         }
     }
